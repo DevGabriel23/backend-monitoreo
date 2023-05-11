af48217aabc0192799b93bccb5f2a9fbbde0fdf5
Gabriel Ramón Cupil <103237220+DevGabriel23@users.noreply.github.com>
2023-05-11 07:23:08 +0000

    Update database.js

--- src/database.js
+++ src/database.js
@@ -4,10 +4,11 @@ const {promisify} = require('util');
 
 let connection = 
 mysql.createPool({
-    host: process.env.HOST,
-    user: process.env.USER,
-    password: process.env.PASSWORD,
-    database: process.env.DATABASE
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    port: process.env.DB_PORT
 });
 
 connection.getConnection((err, connection) => {
